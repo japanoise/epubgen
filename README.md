eb3c4d70f8cea42c7915d3ec8cdbe16afeb2bc9f
japanoise <japanoise@tfwno.gf>
2019-02-18 16:16:59 +0000

    Initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# epubgen - generate simple epubs
+
+This program generates epubs given xhtml and a config file. Run it once to get a
+template, and again to generate the epub - or take a look at the example project
+in `test/`, which additionally uses the provided `md2xhtml` script to generate
+the html from markdown sources.
+
+## copying
+
+Licensed MIT.
---
+++ test/config.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# This is epubgen's configuration file. It is a bash script and will be sourced
+# when epubgen runs.
+
+# Final filename
+EPUBTARGET="test.epub"
+
+# Metadata for content.opf
+EPUBTITLE="My Super Novel"
+EPUBAUTHOR="Jane Doe"
+EPUBFILEAS="Doe, Jane"
+# Use a url instead of an isbn?
+EPUBUSEURL=true
+EPUBISBN="123456789X"
+EPUBURL="http://site.for.my.book/book.htm"
+# Lang must conform to https://www.ietf.org/rfc/rfc3066.txt
+# I.E. [ISO 639-2 or -1 code](-[variant]) - e.g eo, eng-gb
+EPUBLANG="en"
+# Mime-type for chapters - I think it has to be xhtml, but I'm not certain.
+EPUBMIME="application/xhtml+xml"
+
+# List of chapters. Modify the regex to suit your naming scheme; it currently
+# assumes you're using a naming scheme like [0-9]*.html. Order matters as it
+# will use this order to generate the table of contents. If you're doing 
+# something funky, you can just set this to a list of files seperated by '\n's
+EPUBSRCS=$(find ./ -regex ".*[0-9]*.html" -not -path "*build/*" | sort)
+
+# Command to generate tempfiles. If you don't have mktemp, you can use something
+# like 'date +%s | xargs printf "/tmp/tmp.%s\n"'
+maketemp() {
+	mktemp
+}
+
+# Command to generate a chapter title. As-is, it will use the positional order.
+# It is passed the basename, filename, and positional order.
+gentitle() {
+	echo "Chapter $3"
+}
+
+# Command to validate epub at the end of the run. It will be passed the path to 
+# the epub.
+validate() {
+	echo "No validator setup"
+#	java -jar /path/to/epubcheck.jar "$1"
+}
